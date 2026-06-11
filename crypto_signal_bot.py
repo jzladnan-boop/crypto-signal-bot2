777c589b347f01b3660b7fd398aabc67b71baf71
jzladnan-boop <jzladnan@gmail.com>
2026-06-11 11:52:18 +0300

    Update crypto_signal_bot.py

--- crypto_signal_bot.py
+++ crypto_signal_bot.py
@@ -39,6 +39,10 @@ RSI_BUY      = 30
 RSI_SELL     = 70
 CHECK_EVERY  = 60
 
+TELEGRAM_TOKEN   = "8912210093:AAFV9iO98UMgIMGJomHmNTdwM31lCABuLt8"
+TELEGRAM_CHAT_ID = "-1003541055173"
+
+
 # ──────────────────────────────────────────────
 logging.basicConfig(
     level=logging.INFO,
